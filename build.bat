7cc5d14dc3b1f4f6b94c2483800e248f9ac71cbe
DanielKrottendorfer <krotttendorfer@hotmail.de>
2022-02-25 14:33:36 +0100

    moving to CMake

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 
-g++ -o FriendlyHareEngine.dll -s -shared src/FriendlyHareEngine.cpp -Wl,--subsystem,windows
-g++ -o game.exe test.cpp -I inc -L . -l FriendlyHareEngine
-.\game.exe
+rmdir /S build
+mkdir build
+cd build
+
+cmake .. -G "MinGW Makefiles"
+mingw32-make
+
+set "PATH=%PATH%%cd%\src;"
+
+.\TestGame.exe
